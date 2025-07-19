46b45eaad4d39c81965c475387982aa06c96e69a
Enes Mücahit Yıldıran <emyildiran@192.168.1.12>
2025-07-19 13:15:46 +0300

    fix: Autoprefixer production dependency eklendi

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -30,7 +30,7 @@
 /******/ 		// Execute the module function
 /******/ 		var threw = true;
 /******/ 		try {
-/******/ 			__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
 /******/ 			threw = false;
 /******/ 		} finally {
 /******/ 			if(threw) delete __webpack_module_cache__[moduleId];
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("937240e329d2c77e")
+/******/ 		__webpack_require__.h = () => ("0e1c43a1b34474b2")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9dfc44ef5b139c4d"; }
+/******/ 		__webpack_require__.h = function() { return "0bbe92a24073ac96"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
